b6717189b5291c5a6a3749bc7577647f9cbc940b
andrew-pahomov <andrew-pahomov@yandex.ru>
2023-08-21 06:48:47 +0300

    update project

--- src/test/java/ru/netology/selenide/AppCardDeliveryTaskOneTest.java
+++ src/test/java/ru/netology/selenide/AppCardDeliveryTaskOneTest.java
@@ -1,6 +1,7 @@
 package ru.netology.selenide;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -21,15 +22,15 @@ public class AppCardDeliveryTaskOneTest {
     public void shouldBeSuccessfullyCompleted() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Рязань");
-        String currentDate = generateDate(4, "dd.MM.yyyy");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").sendKeys(currentDate);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иванов-Иваныч Иван");
         $("[data-test-id='phone'] input").setValue("+78005553535");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
